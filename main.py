d32d71dea0351b69ede025a6f18b2010d99710d1
Berkay <berkaykasin@gmail.com>
2024-07-14 12:19:56 +0100

    Finished the Project

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from TicTacToeEngine import TicTacToe
 
 
 tictactoe = TicTacToe()
-while True:
-    tictactoe.check_winner()
-    tictactoe.get_user_input()
+game_on = True
+while game_on:
+    game_on = tictactoe.take_go()
+
+
+
